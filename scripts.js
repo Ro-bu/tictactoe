a7e89c4ae9ffef86f6f935aa86f6d4f1b28e0f10
Ro-bu <robin.raid1@gmail.com>
2022-03-22 15:43:44 +0200

    added functions to start the game and add X's O's on the game grid

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 // Gameboard function tracks the X's and O's in the grid
 // tracks game state which means if anyone has won or it's a tie
+// tracks whos turn it is
 
 const gameBoard = (() =>{
     const gameGrid = ["", "", "", "", "", "", "", "", ""];
@@ -36,17 +37,49 @@ const gameBoard = (() =>{
         playerIcon
     }
 })();
+// displays the contents of the grid array
+// adds events on the grid boxes which allow players to write X's O's, and the restart button
+// Controls text content above game grid
+// Initializes the game
 
 const displayController = (() =>{
     const displayGrid = () => {
         for (let i = 0; i<9; i++){
             let gridTile = `[data-index='${i}']`;
-            document.querySelector(gridTile).textContent = gameBoard.checkIndex();
+            document.querySelector(gridTile).textContent = gameBoard.checkIndex(i);
         }
     }
-    return{
-        displayGrid
+    const playerTurn = (a) => {
+        if (gameBoard.checkIndex(a) == ""){
+            gameBoard.setIndex(a);
+            displayGrid();
+            messageHandler();
+        }
     }
 
+    const initialize = () => {
+        gameBoard.reset();
+        displayGrid();
+        messageHandler();
 
+        let gridTile = document.querySelectorAll(".game-grid")
+        gridTile.forEach(grid => {
+            grid.addEventListener("click", () => {
+                playerTurn(grid.dataset.index)
+            })
+        })
+
+        const restartButton = document.querySelector("#restart");
+        restartButton.addEventListener("click", ()=>{
+            initialize();
+        })
+    }
+    const messageBox = document.querySelector(".game-messages");
+    const messageHandler = () => {
+        messageBox.textContent = `Player ${gameBoard.playerIcon()}'s turn`
+    }
+    return{
+        initialize
+    }
 })();
+displayController.initialize();
