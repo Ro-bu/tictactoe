efb18eafa12b3ab94a4d1b8dd858ae3117267804
Ro-bu <robin.raid1@gmail.com>
2022-03-22 23:05:41 +0200

    added winner validation

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 // tracks whos turn it is
 
 const gameBoard = (() =>{
-    const gameGrid = ["", "", "", "", "", "", "", "", ""];
+    let gameGrid = ["", "", "", "", "", "", "", "", ""];
 
     let counter = 1;
     
@@ -19,8 +19,12 @@ const gameBoard = (() =>{
     }
 
     const setIndex = (a) => {
-        gameGrid[a] = playerIcon();
-        counter++;
+        if(! checkWinner()){
+            gameGrid[a] = playerIcon();
+            if(! checkWinner()){
+                counter++;
+            }
+        }
     }
 
     const playerIcon = () => {
@@ -30,11 +34,40 @@ const gameBoard = (() =>{
             return "X"
         }
     }
+
+    const checkTie = () =>{
+        if(counter === 10){
+            return true;
+        } else {
+            return false;
+        }
+    }
+//  for each winning combination
+// check if the gameGrid array contains X's at those indexes
+    const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                                [0, 4, 8], [2, 4, 6]];
+    const checkWinner = () => {
+        let winnerKnown = false;
+        for (let i = 0; i < winningCombinations.length; i++){
+            let currentCondition = winningCombinations[i];
+            let a = gameGrid[currentCondition[0]];
+            let b = gameGrid[currentCondition[1]];
+            let c = gameGrid[currentCondition[2]];
+            if ( a === b && b === c && a !== "" && b!== "" && c !== ""){
+                winnerKnown = true;
+                break;
+            }
+        }
+        return winnerKnown;
+    }
     return {
         reset,
         checkIndex,
         setIndex,
-        playerIcon
+        playerIcon,
+        checkWinner,
+        checkTie
     }
 })();
 // displays the contents of the grid array
@@ -76,7 +109,14 @@ const displayController = (() =>{
     }
     const messageBox = document.querySelector(".game-messages");
     const messageHandler = () => {
-        messageBox.textContent = `Player ${gameBoard.playerIcon()}'s turn`
+        if(gameBoard.checkWinner()){
+            messageBox.textContent = `Player ${gameBoard.playerIcon()} has won the game!`
+        }
+        else if(gameBoard.checkTie()){
+            messageBox.textContent = "It's a tie!"
+    }   else {
+            messageBox.textContent = `Player ${gameBoard.playerIcon()}'s turn`
+        }
     }
     return{
         initialize
